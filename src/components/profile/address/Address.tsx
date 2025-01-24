33ebafc78f4329c024f2a90a97e611875e29b428
Mohsen Baghaei <mohsen.bhi99@gmail.com>
2025-01-24 22:36:21 +0330

    adding routing to address

--- src/components/profile/address/Address.tsx
+++ src/components/profile/address/Address.tsx
@@ -2,7 +2,12 @@ import { ReactElement } from "react";
 
 const Address = (): ReactElement => {
   return (
-    <div className="flex flex-col items-center justify-center w-full gap-7"></div>
+    <div className="flex flex-col items-center justify-center w-full gap-7">
+      <div className="flex justify-between items-center">
+        <p className="text-2xl">Addresses</p>
+        <button>Add New Address</button>
+      </div>
+    </div>
   );
 };
 
