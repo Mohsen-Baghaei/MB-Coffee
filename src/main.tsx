98271bcbd5fad62f1c3bd9c4430f87996f2ee598
Mohsen Baghaei <mohsen.bhi99@gmail.com>
2025-02-03 14:54:48 +0330

    done

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "./App.tsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
 import { fetchProducts } from "./app/products/productsSlice.ts";
@@ -12,10 +12,10 @@ store.dispatch(fetchProducts());
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
